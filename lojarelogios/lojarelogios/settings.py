313d2377302f6dfe23d6520d819ac0404210a7ae
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-18 22:26:08 -0300

    URL Dispatcher, settings e views configuradas

--- lojarelogios/lojarelogios/settings.py
+++ lojarelogios/lojarelogios/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lojarelogiosapp.apps.LojarelogiosappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
