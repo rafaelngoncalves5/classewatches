f2c8299f7ea7f5c214b9ba481410972f64788dd2
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-21 10:03:04 -0300

    Adicionando templates

--- lojarelogios/lojarelogiosapp/views.py
+++ lojarelogios/lojarelogiosapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("ola mundo!")
+    return render(request, 'lojarelogiosapp/index.html')
+
+def products(request):
+    return render(request, 'lojarelogiosapp/products.html')
