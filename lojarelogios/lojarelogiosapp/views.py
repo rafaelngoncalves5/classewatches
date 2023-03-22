6395f83228c78b5f0b5e1ac6258807fafe421749
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-22 08:52:36 -0300

    Model de produtos

--- lojarelogios/lojarelogiosapp/views.py
+++ lojarelogios/lojarelogiosapp/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 def index(request):
     return render(request, 'lojarelogiosapp/index.html')
 
 def products(request):
-    return render(request, 'lojarelogiosapp/products.html')
+    return render(request, 'lojarelogiosapp/products.html', {'produtos': Produto.objects.all()})
