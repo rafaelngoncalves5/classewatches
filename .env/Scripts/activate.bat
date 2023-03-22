9c61e2df8fa4fd2fee93b7d0113885ac84927bd2
Gxavier7 <103543979+Gxavier7@users.noreply.github.com>
2023-03-21 22:11:02 -0300

    Algumas mudanças

--- .env/Scripts/activate.bat
+++ .env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Rafael N\loja-relogios\.env
+set VIRTUAL_ENV=C:\Programação\Projetos Trio\e-commerce\loja-relogios\.env
 
 if not defined PROMPT set PROMPT=$P$G
 
