617ba729d0cad4c4b2fc1ef4db60cdc6beabf0d1
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-23 09:16:49 -0300

    Básico da página de cadastro feito

--- lojarelogios/lojarelogiosapp/urls.py
+++ lojarelogios/lojarelogiosapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name='lojarelogiosapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('products/', views.products, name='products'),
-    path('user/register', views.register, name='register')
+    path('products', views.products, name='products'),
+    path('user/register', views.register, name='register'),
+    path('user/login', views.login, name='login'),
+    path('user/logout', views.logout, name='logout'),
 ]
--- lojarelogios/lojarelogiosapp/views.py
+++ lojarelogios/lojarelogiosapp/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from datetime import timezone
+from django.db import IntegrityError
+from django.shortcuts import redirect, render
+from django.urls import reverse
 from .models import Produto
+from django.contrib.auth.models import User
+from django.contrib.auth import logout
 
 # Create your views here.
 def index(request):
@@ -8,7 +13,34 @@ def index(request):
 def products(request):
     return render(request, 'lojarelogiosapp/products.html', {'produtos': Produto.objects.all()})
 
-
 # Usuário
 def register(request):
+    if request.method == 'POST':
+              primeiro_nome = request.POST['primeiro_nome']
+              ultimo_nome = request.POST['ultimo_nome']
+              usuario = request.POST['usuario']
+              email = request.POST['email']
+              senha = request.POST['senha']
+
+              try:
+                     new_user = User.objects.create_user(f"{usuario}", f"{email}", f"{senha}")
+                     new_user.first_name = primeiro_nome
+                     new_user.last_name = ultimo_nome
+                     new_user.date_joined = timezone.now()
+                     #new_user.get_username
+                     new_user.save()
+                     return redirect(reverse('lojarelogiosapp:index'))
+
+
+              except IntegrityError:
+                     # Tratar isso aqui na fase de testes
+                     pass
+
     return render(request, 'lojarelogiosapp/user/register.html')
+
+def login(request):
+    return render(request, 'lojarelogiosapp/user/login.html')
+
+def logout(request):
+    logout(request)
+    return redirect('/')
