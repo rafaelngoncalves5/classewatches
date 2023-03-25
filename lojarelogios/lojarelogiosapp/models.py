e6a7e3c60c4423f7bca5eec2b28817bc75707b8f
Rafael N <rafaelngoncalves5@outlook.com>
2023-03-25 13:11:51 -0300

    DER e models Fix

--- lojarelogios/lojarelogiosapp/models.py
+++ lojarelogios/lojarelogiosapp/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Carrinho(models.Model):
+    id_carrinho = models.AutoField(primary_key=True)
+    fk_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField(null=True)
+    frete = models.FloatField(null=True, default=0.00)
+
+    def __str__(self):
+        return "Carrinho do usuário " + self.fk_usuario
+
 class Produto(models.Model):
     id_produto = models.AutoField(primary_key=True)
+    fk_carrinho = models.OneToOneField(Carrinho, null=True, on_delete=models.CASCADE)
     titulo = models.CharField(max_length=50)
     descricao = models.CharField(max_length=150)
     preco = models.FloatField(default=0.00)
@@ -16,13 +25,3 @@ class Produto(models.Model):
 
     def __str__(self):
         return self.titulo
-    
-class Carrinho(models.Model):
-    id_carrinho = models.AutoField(primary_key=True)
-    fk_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
-    fk_produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
-    total = models.FloatField(null=True)
-    frete = models.FloatField(null=True, default=0.00)
-
-    def __str__(self):
-        return "Carrinho do usuário " + self.fk_usuario
