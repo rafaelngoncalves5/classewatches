cd767a0c32cbd22bc095078530783e63a0875ed2
Rafael N <rafaelngoncalves5@outlook.com>
2023-05-07 11:09:37 -0300

    Deploy da aplicação

--- lojarelogios/wsgi.py
+++ lojarelogios/wsgi.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from lojarelogios import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lojarelogios.settings')
 
 application = get_wsgi_application()
