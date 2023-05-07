869c9f5a1e3c84ddf86c1094b3d36a8bcb930822
Rafael N <rafaelngoncalves5@outlook.com>
2023-05-07 09:53:35 -0300

    Deploy da aplicação

---
+++ lojarelogios/lojarelogiosapp/env.py
@@ -0,0 +1,11 @@
+mp_sdk = "TEST-6033284383326765-042517-f71f881b0fdb00736ff2f02a4c8360ac-472353305"
+
+email_host_user = "rafaelngoncalves5@outlook.com"
+email_host_pass = "topacify10br923"
+
+stripe_webhook = "pk_test_51MvSb4KY6ADNgA35DTOBFIYem4ERNdNTRyZkOw4xDQH9lADWv7WlsxEvkpRVGEIBaVMuslxRyyVllnO7aqD9qw7t00m3vQcW0b"
+stripe_skey = "sk_test_51MvSb4KY6ADNgA35uY1XBR6tLuYpAHKBHjti691qRJhFxknWLEJSuQERgbMzNo4sXmERkegLNlOIDsUfXTuw97nV00wpFMR8xe"
+
+django_skey = "'django-insecure-ltoe#8p)2&tj9xl@#7pv(ghwoc%!qm5(pzr2mp**a*11!&)n#o'"
+
+psql_pass = 'birdscooter123'
