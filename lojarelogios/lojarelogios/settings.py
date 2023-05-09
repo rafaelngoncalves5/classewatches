fd4771887f737de76d040ac3393e9819b387791e
Rafael N <rafaelngoncalves5@outlook.com>
2023-05-09 12:25:57 -0300

    Staticfiles

--- lojarelogios/lojarelogios/settings.py
+++ lojarelogios/lojarelogios/settings.py
@@ -137,12 +137,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'lojarelogiosapp/static'),)
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
