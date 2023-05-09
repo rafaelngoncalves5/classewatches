27f42cf2b0d4c9e84ed2b95088c385934ba9d947
Rafael N <rafaelngoncalves5@outlook.com>
2023-05-09 16:07:04 -0300

    Fix

--- lojarelogios/lojarelogios/settings.py
+++ lojarelogios/lojarelogios/settings.py
@@ -39,7 +39,6 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'lojarelogiosapp.apps.LojarelogiosappConfig',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
